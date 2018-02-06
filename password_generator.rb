e6b24cf53051c721438221586d0e212657ab016a
Joe OBrien <joe@objo.com>
2018-02-05 20:27:18 -0500

    finishing up password_generator

--- password_generator.rb
+++ password_generator.rb
@@ -1,4 +1,5 @@
 # - Have the user enter a string 
+
 # - Swap out the letters for numbers as follows:
 #    a == 4
 #    e == 3
@@ -7,8 +8,21 @@
 #    t == 7
 # 
 # - Remove spaces 
+#
 # - capitalize words
-# - add a random special character (*&^%$#@, etc.)
+#
+### - add a random special character (*&^%$#@, etc.)
+
+TRANSLATIONS = {
+  "a" => "4",
+  "e" => "3",
+  "i" => "1",
+  "o" => "0",
+  "t" => "7"
+}
+
+SPECIAL_CHARACTERS = ["@", "!", "%", "&", "*", ")", "("]
+
 puts "Password Generator\n\n"
 
 puts "Please enter a phrase to convert"
@@ -21,10 +35,35 @@ original_phrase_as_array.each do |word|
   word.capitalize!
 end
 
-new_passphrase = original_phrase_as_array.join
+new_passphrase_array = original_phrase_as_array.join.split("")
+
+final_passphrase = ""
+
+new_passphrase_array.collect do |letter|
+
+  if TRANSLATIONS[letter.downcase].nil?
+    final_passphrase << letter 
+  else
+    final_passphrase << TRANSLATIONS[letter.downcase]
+  end
+
+  # case letter.downcase
+  # when "a" then final_passphrase << "4"
+  # when "e" then final_passphrase << "3"
+  # when "i" then final_passphrase << "1"
+  # when "o" then final_passphrase << "0"
+  # when "t" then final_passphrase << "7"
+  # else          final_passphrase << letter  
+  # end 
+
+end
+
+position_of_special_character = rand(final_passphrase.length - 1)
+
+final_passphrase = final_passphrase.insert(position_of_special_character, SPECIAL_CHARACTERS.sample)
 
 puts "Original phrase is #{original_phrase}"
-puts "New Passphrase is #{new_passphrase}"
+puts "New Passphrase is #{final_passphrase}"
 
 
 
